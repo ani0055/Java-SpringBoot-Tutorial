42ac4b5b50a02378a6802ad7bba7925b9f228a51
Animesh Gawhale <animeshgawhale236@gmail.com>
2026-03-11 19:49:35 +0530

    Removed old Directory

---
+++ spring_rest/src/main/java/com/animesh/spring_rest/StudentController.java
@@ -0,0 +1,35 @@
+// package com.animesh.spring_rest;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+// @RestController
+// public class StudentController {
+//     @RequestMapping("stud", method=RequestMethod.GET)
+//     public String requestMethodName(@RequestParam String param) {
+//         return new String();
+//     }
+    
+//     public List<Student> getStudent(){
+//         List<Student> stud = new ArrayList<>();
+
+//         Student s1 = new Student();
+//         s1.setS_roll_no(101);
+//         s1.setS_name("Avni Mathur");
+//         s1.setS_marks(89);
+
+//         Student s2 = new Student();
+//         s2.setS_roll_no(102);
+//         s2.setS_name("Raghav Saxsena");
+//         s2.setS_marks(72);
+
+
+//         return stud;
+//     }
+// }
